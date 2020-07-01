65806ec600a1664c3e405240ba83a9fc66e2e824
Felipe Damasceno <damassaa@gmail.com>
2020-07-01 20:51:16 -0300

    Modal creation

--- src/components/Modal/ModalStyle.js
+++ src/components/Modal/ModalStyle.js
@@ -1,63 +1,13 @@
 import styled from 'styled-components';
 
-import CloseIcon from '../../assets/icons/close.svg';
-
-export const ModalWrapper = styled.div`
-    position: fixed;
-    display: none;
-    top: 0%;
-    left: 0;
-    height: 100vh;
-    width: 100vw;
-    justify-content: center;
+export const modal = styled.div`
+    display: flex;
     align-items: center;
-    background-color: rgba(0,0,0, 0.5);
+    justify-items: center;
 `
 
-export const Modal = styled.div`
-    position: relative;
-    display: flex;
-    height: ${props => props.height ? props.height : 'auto'};
-    width: ${props => props.width ? props.width : '450px'};
-    max-width: 95%;
-    flex-flow: column nowrap;
-    align-items: center;
-    border-radius: 10px;
+export const paper = styled.div`
     background-color: #fff;
-    box-shadow: 3px 3px 5px 0px rgba(0,0,0,0.25);
-`
-
-export const CloseModal = styled.div`
-    display: flex;
-    width: 94%;
-    justify-content: flex-end;
-    padding: 3%;
-`
-
-export const CloseModalIcon = styled.div`
-    height: 20px;
-    width: 20px;
-    right: 0;
-    background-image: url(${CloseIcon});
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: contain;
-    cursor: pointer;
-`
-
-export const ModalTitle = styled.div`
-    margin-top: -20px;
-    margin-bottom: 2vh;
-    font-size: 24px;
-    font-weight: bold;
-    color: var(--color-1);
-`
-
-export const ModalContent = styled.div`
-    display: flex;
-    width: 100%;
-    flex-flow: column nowrap;
-    align-items: center;
-    margin-bottom: 25px;
-    color: #000;
+    border: 2px solid #000;
+    padding: 8px 16px;
 `
--- src/components/pages/Champions/index.js
+++ src/components/pages/Champions/index.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import {makeStyles} from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 import Wrapper from "../../Wrapper/style";
 import FooterWrapper from "../../Footer/";
 import { dataChampions as TextWrapper } from "../../Text";
@@ -10,8 +14,33 @@ import {
   ChampionName,
 } from "../Champions/style";
 
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 const Champions = () => {
+  const classes = useStyles();
   const [data, setData] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [active, setActive] = useState({});
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  }
 
   useEffect(() => {
     axios
@@ -30,13 +59,32 @@ const Champions = () => {
         <TextWrapper />
         <CWrapper>
           {data.map((champions, id) => (
-            <ChampionCard key={id}>
+            <ChampionCard key ={id} onClick={() => {handleOpen(); setActive(champions)}}>
               <ChampionCardImage image={champions.image} />
               <ChampionName>
                 <span>{champions.name}</span>
               </ChampionName>
             </ChampionCard>
           ))}
+          <Modal
+              aria-labelledby="transition-modal-title"
+              aria-describedby="transition-modal-description"
+              className={classes.modal}
+              open={open}
+              onClose={handleClose}
+              closeAfterTransition
+              BackdropComponent={Backdrop}
+              BackdropProps={{
+                timeout: 500,
+              }}
+          >
+            <Fade in={open}>
+              <div className={classes.paper}>
+                <h2 id="transition-modal-title">{active.name}</h2>
+                <p id="transition-modal-description">{active.nickname}</p>
+              </div>
+            </Fade>
+          </Modal>
         </CWrapper>
       </Wrapper>
       <FooterWrapper />
